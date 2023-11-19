39198753f3839744c55983dfe15d8e4ed2a78da2
nao mac <sagalumaata@gmail.com>
2023-11-19 15:51:16 +0900

    input変更時、再描画が走らないようuseRefに変更

--- src/components/Memo/Header.tsx
+++ src/components/Memo/Header.tsx
@@ -1,38 +1,38 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState,memo,useRef } from "react";
 import {createUUID} from "../../utils/createUUID"
 
 
 export const Header:React.FC = () => {
-	console.log('処理が走る')
 
 	let first_uuid = createUUID();
-	const [inputed_UUID,setUUID] = useState("")
-
+	const inputed_UUID = useRef<HTMLInputElement>(null);
 	const [login_disable,set_login_disable] = useState(false);
 
 	// ログイン処理
 	const LoginHundler = ()=>{
-		fetch('https://challenge-server.tracks.run/memoapp/category', {
-			method: 'GET',
-			headers: {
-				'x-access-token':inputed_UUID,
-				'Content-Type': 'application/json'
-			}
-		})
-		.then(response => {
-			// レスポンスを処理するコードを記述
-			console.log(response)
-		})
+		if (inputed_UUID.current) {
+			fetch('https://challenge-server.tracks.run/memoapp/category', {
+				method: 'GET',
+				headers: {
+					'x-access-token':inputed_UUID.current?.value,
+					'Content-Type': 'application/json'
+				}
+			})
+			.then(response => {
+				// レスポンスを処理するコードを記述
+				console.log(response)
+			})
+		}
 	}
 	return(
 		<header>
 			<input type="text" 
 				id="access_token" 
 				placeholder={first_uuid}
-				onChange={(e) => setUUID(e.target.value)}
-			/>
+				ref={inputed_UUID}
+				/>
 			<button disabled={login_disable} onClick={LoginHundler}>Login</button>
 		</header>
 	)
-
+	
 }
--- src/components/Memo/MemoMain.tsx
+++ src/components/Memo/MemoMain.tsx
@@ -1,6 +1,5 @@
 import { Header } from "../../components/Memo/Header"
 
-
 export const MemoMain:React.FC = () => {
 	return(
 		<>
