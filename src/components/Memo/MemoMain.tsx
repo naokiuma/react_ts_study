9da6c4307b5439160914f1fb279ca3040a2bb47a
nao mac <sagalumaata@gmail.com>
2023-11-19 15:49:02 +0900

    input変更時、再描画が走らないようuseRefに変更

--- src/components/Memo/MemoMain.tsx
+++ src/components/Memo/MemoMain.tsx
@@ -1,7 +1,11 @@
 import { Header } from "../../components/Memo/Header"
+import {createUUID} from "../../utils/createUUID"
 
 
 export const MemoMain:React.FC = () => {
+	let first_uuid = createUUID();
+
+
 	return(
 		<>
 			<Header/>
