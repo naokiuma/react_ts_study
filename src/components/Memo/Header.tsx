6a4fc11d0e3b588cdc103b3514cb9a66c5586617
nao mac <sagalumaata@gmail.com>
2023-11-20 23:19:47 +0900

    ログインできたらカテゴリーを画面に表示

--- src/components/Memo/Header.tsx
+++ src/components/Memo/Header.tsx
@@ -1,8 +1,16 @@
 import { useEffect, useState,memo,useRef } from "react";
 import {createUUID} from "../../utils/createUUID"
 
+type HeaderProps = {
+	setCategory: any
+};
+type Data = {
+	value: any; // valueプロパティの型を適切に指定
+	// 他のプロパティがあればここに追加
+};
 
-export const Header:React.FC = () => {
+
+export const Header:React.FC<HeaderProps> = (props) => {
 
 	let first_uuid = createUUID();
 	const inputed_UUID = useRef<HTMLInputElement>(null);
@@ -14,13 +22,18 @@ export const Header:React.FC = () => {
 			fetch('https://challenge-server.tracks.run/memoapp/category', {
 				method: 'GET',
 				headers: {
-					'x-access-token':inputed_UUID.current?.value,
+					// 'x-access-token':inputed_UUID.current?.value,
+					'x-access-token':first_uuid,
 					'Content-Type': 'application/json'
 				}
 			})
 			.then(response => {
 				// レスポンスを処理するコードを記述
-				console.log(response)
+				return response.json()
+			})
+			.then(data => {
+				console.log('取得したデータ:', data); // 取得したデータを表示			
+				props.setCategory(data); // データをpropsに渡す例
 			})
 		}
 	}
--- src/components/Memo/MemoMain.tsx
+++ src/components/Memo/MemoMain.tsx
@@ -1,13 +1,27 @@
+import { useState } from "react"
 import { Header } from "../../components/Memo/Header"
 import {createUUID} from "../../utils/createUUID"
 
+type Category = {
+	id: number; // または適切な型
+	name: string; // または適切な型
+  };
+
 export const MemoMain:React.FC = () => {
-	let first_uuid = createUUID();
-
-
+	const [category,setCategory] = useState<Category[]>([])
 	return(
 		<>
-			<Header/>
+			<Header setCategory={setCategory}/>
+			{category.length > 0 && 
+				<ul>
+					{
+						category.map((item, index) => (
+							<li key={index}>{item.name}</li>
+						))
+					}
+				</ul>
+			}
+
 		</>
 
 	)
